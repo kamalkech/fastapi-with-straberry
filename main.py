8bd5946085d22e7138cbfedd7a1ef3baff85909d
kamal <maroc.develop@gmail.com>
2023-10-31 01:40:38 +0100

    .

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 """..."""
+from typing import Annotated
+from datetime import timedelta
+
 import strawberry
 from strawberry.fastapi import GraphQLRouter
 
 import uvicorn
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.auth.auth import (
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    authenticate_user,
+    create_access_token,
+    fake_users_db,
+    get_current_active_user,
+    Token,
+    User
+)
 
 from app.core.query import Query
 from app.core.mutation import Mutation
-
 
 schema = strawberry.Schema(query=Query, mutation=Mutation)
 graphql_app = GraphQLRouter(schema)
@@ -33,6 +46,40 @@ async def root():
     return {"message": "Hello World!"}
 
 app.include_router(graphql_app, prefix='/graphql')
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(
+        fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return current_user
+
+
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=3333)
