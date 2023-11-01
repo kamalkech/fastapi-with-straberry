a6e64af6463b76f950fb89fbb819c038dc3b43bd
kamal <maroc.develop@gmail.com>
2023-11-01 19:51:26 +0100

    .

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,8 +1,10 @@
+"""..."""
+
 from datetime import datetime, timedelta
 from typing import Annotated
 
 from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
@@ -26,15 +28,18 @@ fake_users_db = {
 
 
 class Token(BaseModel):
+    """..."""
     access_token: str
     token_type: str
 
 
 class TokenData(BaseModel):
+    """..."""
     username: str | None = None
 
 
 class User(BaseModel):
+    """..."""
     username: str
     email: str | None = None
     full_name: str | None = None
@@ -42,6 +47,7 @@ class User(BaseModel):
 
 
 class UserInDB(User):
+    """..."""
     hashed_password: str
 
 
@@ -53,29 +59,40 @@ app = FastAPI()
 
 
 def verify_password(plain_password, hashed_password):
+    """..."""
     return pwd_context.verify(plain_password, hashed_password)
 
 
 def get_password_hash(password):
+    """..."""
     return pwd_context.hash(password)
 
 
 def get_user(db, username: str):
+    """..."""
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
 
 
-def authenticate_user(fake_db, username: str, password: str):
+def authenticate_user(fake_db, username: str, password: str) -> User:
+    """..."""
     user = get_user(fake_db, username)
     if not user:
-        return False
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User not found!"
+        )
     if not verify_password(password, user.hashed_password):
-        return False
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Password not correct!"
+        )
     return user
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """..."""
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -87,6 +104,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    """..."""
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
---
+++ app/utils.py
@@ -0,0 +1,67 @@
+"""..."""
+
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from typing import Optional, Union, Any
+from jose import jwt
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 days
+ALGORITHM = "HS256"
+# os.environ['JWT_SECRET_KEY']   # should be kept secret
+JWT_SECRET_KEY = "my-secret"
+# os.environ['JWT_REFRESH_SECRET_KEY']    # should be kept secret
+JWT_REFRESH_SECRET_KEY = "my-secret-refresh"
+
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_hashed_password(password: str) -> str:
+    """..."""
+    return password_context.hash(password)
+
+
+def verify_password(password: str, hashed_pass: str) -> bool:
+    """..."""
+    return password_context.verify(password, hashed_pass)
+
+
+def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
+    return encoded_jwt
+
+
+def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, JWT_REFRESH_SECRET_KEY, ALGORITHM)
+    return encoded_jwt
+
+
+def decode_token(token: str):
+    payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
+    print("payload", payload)
+
+
+list_users = [
+    {
+        "username": "root",
+        "email": "root@email.com",
+        "password": get_hashed_password("root")
+    },
+    {
+        "username": "admin",
+        "email": "admin@email.com",
+        "password": get_hashed_password("admin")
+    }
+]
